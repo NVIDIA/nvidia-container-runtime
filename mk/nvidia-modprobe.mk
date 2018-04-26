4d436659d844aab04ea5ac05fb9381d5c1ec3caa
Jonathan Calmels <jcalmels@nvidia.com>
2018-04-26 15:32:53 -0700

    Bump nvidia-modprobe-utils to 396.18

--- mk/nvidia-modprobe.mk
+++ mk/nvidia-modprobe.mk
@@ -6,7 +6,7 @@ include $(MAKE_DIR)/common.mk
 
 ##### Source definitions #####
 
-VERSION        := 390.25
+VERSION        := 396.18
 PREFIX         := nvidia-modprobe-$(VERSION)
 URL            := https://github.com/NVIDIA/nvidia-modprobe/archive/$(VERSION).tar.gz
 
