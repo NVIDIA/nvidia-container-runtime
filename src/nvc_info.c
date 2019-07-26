b9545d7f9dd5e110bdac4ba5c334d4ccb044ab43
guptaNswati <swatig@nvidia.com>
2019-07-26 14:20:44 -0700

    Add support for Vulkan

--- src/nvc_info.c
+++ src/nvc_info.c
@@ -102,6 +102,7 @@ static const char * const graphics_libs_glvnd[] = {
         "libEGL_nvidia.so",                 /* EGL ICD */
         "libGLESv2_nvidia.so",              /* OpenGL ES v2 ICD */
         "libGLESv1_CM_nvidia.so",           /* OpenGL ES v1 common profile ICD */
+	"libnvidia-glvkspirv.so",           /* SPIR-V Lib for Vulkan */
 };
 
 static const char * const graphics_libs_compat[] = {
