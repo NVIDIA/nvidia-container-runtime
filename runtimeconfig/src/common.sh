f09ab93a74c0ee05352019892af2d47fd50062e1
Renaud Gaubert <rgaubert@nvidia.com>
2019-09-19 16:34:50 -0700

    Refactor runtimeconfig
    Signed-off-by: Renaud Gaubert <rgaubert@nvidia.com>

--- runtimeconfig/src/common.sh
+++ runtimeconfig/src/common.sh
@@ -27,3 +27,30 @@ log() {
 	printf "%s[%s]%s %b\n" "${fmt_on}" "${level}" "${fmt_off}" "${message}" >&2
 }
 
+with_retry() {
+	local max_attempts="$1"
+	local delay="$2"
+	local count=0
+	local rc
+	shift 2
+
+	while true; do
+		set +e
+		"$@"; rc="$?"
+		set -e
+
+		count="$((count+1))"
+
+		if [[ "${rc}" -eq 0 ]]; then
+			return 0
+		fi
+
+		if [[ "${max_attempts}" -le 0 ]] || [[ "${count}" -lt "${max_attempts}" ]]; then
+			sleep "${delay}"
+		else
+			break
+		fi
+	done
+
+	return 1
+}
