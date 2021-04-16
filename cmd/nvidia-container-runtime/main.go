c6b0b096f12471584eb603f5fbd0a21004aebd20
Evan Lezar <elezar@nvidia.com>
2021-04-16 13:59:00 +0200

    Refactor container runtime to use new packages
    This change refactors the NVIDIA container runtime to implement
    the oci.Runtime interface. A SyscallExec Runtime is used
    to encapsulate the calls to the underlying runtime (runc) and
    an oci.SpecModifier encapsulates the processing of the
    OCI specification.
    
    Modifications are still implemented in the existing addNVIDIAHooks
    function.
    
    The change also includes refactoring / reorganisation of the
    functions such as parsing of arguments and adds additional
    unit tests.
    
    Signed-off-by: Evan Lezar <elezar@nvidia.com>

--- cmd/nvidia-container-runtime/main.go
+++ cmd/nvidia-container-runtime/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path"
-	"path/filepath"
-	"strings"
-	"syscall"
 
-	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/pelletier/go-toml"
 )
 
@@ -27,151 +21,6 @@ var (
 )
 
 var logger = NewLogger()
-
-type args struct {
-	bundleDirPath string
-	cmd           string
-}
-
-type config struct {
-	debugFilePath string
-}
-
-func getConfig() (*config, error) {
-	cfg := &config{}
-
-	if XDGConfigDir := os.Getenv(configOverride); len(XDGConfigDir) != 0 {
-		configDir = XDGConfigDir
-	}
-
-	configFilePath := path.Join(configDir, configFilePath)
-
-	tomlContent, err := ioutil.ReadFile(configFilePath)
-	if err != nil {
-		return nil, err
-	}
-
-	toml, err := toml.Load(string(tomlContent))
-	if err != nil {
-		return nil, err
-	}
-
-	cfg.debugFilePath = toml.GetDefault("nvidia-container-runtime.debug", "/dev/null").(string)
-
-	return cfg, nil
-}
-
-// getArgs checks the specified slice of strings (argv) for a 'bundle' flag and a 'create'
-// command line argument as allowed by runc.
-// The following are supported:
-// --bundle{{SEP}}BUNDLE_PATH
-// -bundle{{SEP}}BUNDLE_PATH
-// -b{{SEP}}BUNDLE_PATH
-// where {{SEP}} is either ' ' or '='
-func getArgs(argv []string) (*args, error) {
-	args := &args{}
-
-	for i := 0; i < len(argv); i++ {
-		param := argv[i]
-		if param == "create" {
-			args.cmd = param
-			continue
-		}
-
-		if !strings.HasPrefix(param, "-") {
-			continue
-		}
-
-		trimmed := strings.TrimLeft(param, "-")
-		if len(trimmed) == 0 {
-			continue
-		}
-
-		parts := strings.SplitN(trimmed, "=", 2)
-		if parts[0] != "bundle" && parts[0] != "b" {
-			continue
-		}
-
-		if len(parts) == 2 {
-			args.bundleDirPath = parts[1]
-			continue
-		}
-
-		if len(argv)-i <= 1 {
-			return nil, fmt.Errorf("bundle option needs an argument")
-		}
-		args.bundleDirPath = argv[i+1]
-		i++
-	}
-
-	return args, nil
-}
-
-// execRunc discovers the runc binary and issues an exec syscall.
-func execRunc() error {
-	runcCandidates := []string{
-		"docker-runc",
-		"runc",
-	}
-
-	var err error
-	var runcPath string
-	for _, candidate := range runcCandidates {
-		logger.Printf("Looking for \"%v\" binary", candidate)
-		runcPath, err = exec.LookPath(candidate)
-		if err == nil {
-			break
-		}
-		logger.Printf("\"%v\" binary not found: %v", candidate, err)
-	}
-	if err != nil {
-		return fmt.Errorf("error locating runc: %v", err)
-	}
-
-	logger.Printf("Runc path: %s\n", runcPath)
-
-	err = syscall.Exec(runcPath, append([]string{runcPath}, os.Args[1:]...), os.Environ())
-	if err != nil {
-		return fmt.Errorf("could not exec '%v': %v", runcPath, err)
-	}
-
-	// syscall.Exec is not expected to return. This is an error state regardless of whether
-	// err is nil or not.
-	return fmt.Errorf("unexpected return from exec '%v'", runcPath)
-}
-
-func addNVIDIAHook(spec *specs.Spec) error {
-	path, err := exec.LookPath("nvidia-container-runtime-hook")
-	if err != nil {
-		path = hookDefaultFilePath
-		_, err = os.Stat(path)
-		if err != nil {
-			return err
-		}
-	}
-
-	logger.Printf("prestart hook path: %s\n", path)
-
-	args := []string{path}
-	if spec.Hooks == nil {
-		spec.Hooks = &specs.Hooks{}
-	} else if len(spec.Hooks.Prestart) != 0 {
-		for _, hook := range spec.Hooks.Prestart {
-			if !strings.Contains(hook.Path, "nvidia-container-runtime-hook") {
-				continue
-			}
-			logger.Println("existing nvidia prestart hook in OCI spec file")
-			return nil
-		}
-	}
-
-	spec.Hooks.Prestart = append(spec.Hooks.Prestart, specs.Hook{
-		Path: path,
-		Args: append(args, "prestart"),
-	})
-
-	return nil
-}
 
 func main() {
 	err := run(os.Args)
@@ -201,83 +50,41 @@ func run(argv []string) (err error) {
 		logger.CloseFile()
 	}()
 
+	r, err := newRuntime(argv)
+	if err != nil {
+		return fmt.Errorf("error creating runtime: %v", err)
+	}
+
 	logger.Printf("Running %s\n", argv[0])
-	args, err := getArgs(argv)
-	if err != nil {
-		return fmt.Errorf("error getting processing command line arguments: %v", err)
-	}
-
-	if args.cmd != "create" {
-		logger.Println("Command is not \"create\", executing runc doing nothing")
-		err = execRunc()
-		if err != nil {
-			return fmt.Errorf("error forwarding command to runc: %v", err)
-		}
-	}
-
-	configFilePath, err := args.getConfigFilePath()
-	if err != nil {
-		return fmt.Errorf("error getting config file path: %v", err)
-	}
-
-	logger.Printf("Using OCI specification file path: %v", configFilePath)
-
-	jsonFile, err := os.OpenFile(configFilePath, os.O_RDWR, 0644)
-	if err != nil {
-		return fmt.Errorf("error opening OCI specification file: %v", err)
-	}
-
-	defer jsonFile.Close()
-
-	jsonContent, err := ioutil.ReadAll(jsonFile)
-	if err != nil {
-		return fmt.Errorf("error reading OCI specificaiton: %v", err)
-	}
-
-	var spec specs.Spec
-	err = json.Unmarshal(jsonContent, &spec)
-	if err != nil {
-		return fmt.Errorf("error unmarshalling OCI specification: %v", err)
-	}
-
-	err = addNVIDIAHook(&spec)
-	if err != nil {
-		return fmt.Errorf("error injecting NVIDIA Container Runtime hook: %v", err)
-	}
-
-	jsonOutput, err := json.Marshal(spec)
-	if err != nil {
-		return fmt.Errorf("error marshalling modified OCI specification: %v", err)
-	}
-
-	_, err = jsonFile.WriteAt(jsonOutput, 0)
-	if err != nil {
-		return fmt.Errorf("error writing modifed OCI specification to file: %v", err)
-	}
-
-	logger.Print("Prestart hook added, executing runc")
-	err = execRunc()
-	if err != nil {
-		return fmt.Errorf("error forwarding 'create' command to runc: %v", err)
-	}
-
-	return nil
+	return r.Exec(argv)
 }
 
-func (a args) getConfigFilePath() (string, error) {
-	configRoot := a.bundleDirPath
-	if configRoot == "" {
-		logger.Printf("Bundle directory path is empty, using working directory.")
-		workingDirectory, err := os.Getwd()
-		if err != nil {
-			return "", fmt.Errorf("error getting working directory: %v", err)
-		}
-		configRoot = workingDirectory
+type config struct {
+	debugFilePath string
+}
+
+// getConfig sets up the config struct. Values are read from a toml file
+// or set via the environment.
+func getConfig() (*config, error) {
+	cfg := &config{}
+
+	if XDGConfigDir := os.Getenv(configOverride); len(XDGConfigDir) != 0 {
+		configDir = XDGConfigDir
 	}
 
-	logger.Printf("Using bundle directory: %v", configRoot)
+	configFilePath := path.Join(configDir, configFilePath)
 
-	configFilePath := filepath.Join(configRoot, "config.json")
+	tomlContent, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		return nil, err
+	}
 
-	return configFilePath, nil
+	toml, err := toml.Load(string(tomlContent))
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.debugFilePath = toml.GetDefault("nvidia-container-runtime.debug", "/dev/null").(string)
+
+	return cfg, nil
 }
---
+++ cmd/nvidia-container-runtime/runtime_factory_test.go
@@ -0,0 +1,131 @@
+/*
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+*/
+
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetBundlePath(t *testing.T) {
+	type expected struct {
+		bundle  string
+		isError bool
+	}
+	testCases := []struct {
+		argv     []string
+		expected expected
+	}{
+		{
+			argv: []string{},
+		},
+		{
+			argv: []string{"create"},
+		},
+		{
+			argv: []string{"--bundle"},
+			expected: expected{
+				isError: true,
+			},
+		},
+		{
+			argv: []string{"-b"},
+			expected: expected{
+				isError: true,
+			},
+		},
+		{
+			argv: []string{"--bundle", "/foo/bar"},
+			expected: expected{
+				bundle: "/foo/bar",
+			},
+		},
+		{
+			argv: []string{"--not-bundle", "/foo/bar"},
+		},
+		{
+			argv: []string{"--"},
+		},
+		{
+			argv: []string{"-bundle", "/foo/bar"},
+			expected: expected{
+				bundle: "/foo/bar",
+			},
+		},
+		{
+			argv: []string{"--bundle=/foo/bar"},
+			expected: expected{
+				bundle: "/foo/bar",
+			},
+		},
+		{
+			argv: []string{"-b=/foo/bar"},
+			expected: expected{
+				bundle: "/foo/bar",
+			},
+		},
+		{
+			argv: []string{"-b=/foo/=bar"},
+			expected: expected{
+				bundle: "/foo/=bar",
+			},
+		},
+		{
+			argv: []string{"-b", "/foo/bar"},
+			expected: expected{
+				bundle: "/foo/bar",
+			},
+		},
+		{
+			argv: []string{"create", "-b", "/foo/bar"},
+			expected: expected{
+				bundle: "/foo/bar",
+			},
+		},
+		{
+			argv: []string{"-b", "create", "create"},
+			expected: expected{
+				bundle: "create",
+			},
+		},
+		{
+			argv: []string{"-b=create", "create"},
+			expected: expected{
+				bundle: "create",
+			},
+		},
+		{
+			argv: []string{"-b", "create"},
+			expected: expected{
+				bundle: "create",
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		bundle, err := getBundlePath(tc.argv)
+
+		if tc.expected.isError {
+			require.Errorf(t, err, "%d: %v", i, tc)
+		} else {
+			require.NoErrorf(t, err, "%d: %v", i, tc)
+		}
+
+		require.Equalf(t, tc.expected.bundle, bundle, "%d: %v", i, tc)
+	}
+}
